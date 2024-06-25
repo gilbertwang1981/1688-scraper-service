44225da42e715d5bbb0235b34a7bce43c3bc7629
wanghan <wanghan@kp99.cn>
2024-06-25 10:42:13 +0800

    新增账号过期监控程序

---
+++ aliCookieMonitor.py
@@ -0,0 +1,35 @@
+import time
+
+from apscheduler.schedulers.blocking import BlockingScheduler
+import requests
+import json
+import ruamel.yaml
+
+
+yaml = ruamel.yaml.YAML(typ='rt')
+
+with open('aliCookieMonitor.yaml', 'r') as file:
+    config = yaml.load(file)
+
+
+def my_task():
+    for user in config['aliCookie']['users']:
+        data = json.dumps({"offerId": str(config['aliCookie']['offerId']), "userName": str(user)})
+        headers = {'Content-Type': 'application/json'}
+        response = requests.post(url=config['aliCookie']['url'], data=data, headers=headers)
+        robot = config['aliCookie']['robot']
+        alert = {
+            'msgtype': 'text',
+            'text': {
+                'content': str('1688账号') + str(user) + str('异常探测结果：') + str(response.status_code)
+            }
+        }
+        requests.post(robot, json=alert)
+
+
+scheduler = BlockingScheduler()
+
+scheduler.add_job(my_task, 'interval', seconds=config['aliCookie']['interval'])
+
+scheduler.start()
+
